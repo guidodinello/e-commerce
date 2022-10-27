ef49dfa066ead335126d9f2b5c165d0c226de858
Guido Dinello <guido.dinello@gmail.com>
2022-10-27 00:08:33 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,33 @@
+const cartBody = document.getElementById("shopping-cart-body");
+
+const total = document.getElementById("total");
+const subtotal = document.getElementById("subtotal");
+const shippingType = document.getElementsByName("shippingType");
+const shippingCost = document.getElementById("shippingCost");
+
+const paymenyMethod = document.getElementsByName("paymentMethod");
+const paymentMethodMsg = document.getElementById("paymentMethodmsg");
+const creditCardInputs = document.getElementsByClassName("cc-info");
+const bankTransferInputs = document.getElementsByClassName("bt-info");
+
+const alert = document.getElementById("successAlert"); 
+const forms = document.getElementsByClassName("needs-validation");
+
+const successIcon = document.createElement("i");
+successIcon.classList.add("fas", "fal", "fa-check-circle", "ps-2");
+successIcon.style.color = "#00e03c";
+const errorIcon = document.createElement("i");
+errorIcon.classList.add("fas", "fal", "fa-exclamation-circle", "ps-2");
+errorIcon.style.color = "#ff0602";
+
 function getCart(id) {
     return getJSONData(CART_INFO_URL + id + EXT_TYPE);
 }
 
-
-const cartBody = document.getElementById("shopping-cart").getElementsByTagName("tbody")[0];
-
 // se agrega el id del producto a la fila asi puedo borrar el elemento sin necesidad de borrar todos
 // y volverlos a poner
+// me defini el atributo data-old-value para el span asi se que valor habia y puedo sumar la diferencia
+// en vez de sumar devuelta todos los elementos del carrito
 function addRow({ id: id, name: name, count: count, unitCost: unitCost, currency: curr, image: src }) {
     cartBody.insertAdjacentHTML("beforeend",
         `<tr id="item-${id}" class="cart-item">
@@ -16,12 +37,12 @@ function addRow({ id: id, name: name, count: count, unitCost: unitCost, currency
                 <td>${name}</td>
                 <td>${curr} ${unitCost}</td>
                 <td class="d-flex justify-content-center">
-                    <input type="number" class="form-control w-25" aria-label="Product Amount" min="1" value="${count}"
-                    oninput="calcSubtotal(this, ${unitCost})">
+                    <input id="input-item-${id}" type="number" class="form-control w-25" aria-label="Product Amount" min="1" value="${count}"
+                    oninput="updateSubtotal(this, ${unitCost})">
                 </td>
                 <td class="fw-bold">
                 ${curr} 
-                    <span>${unitCost * count}</span>
+                    <span data-oldvalue="${unitCost * count}">${unitCost * count}</span>
                 </td>
                 <td>
                     <button onclick="removeCartItem(${id})" class="btn">
@@ -30,74 +51,97 @@ function addRow({ id: id, name: name, count: count, unitCost: unitCost, currency
                 </td>
         </tr>`);
 
-    subtotal.innerText = parseInt(subtotal.innerText) + unitCost * count;
+    subtotal.innerText = (parseFloat(subtotal.innerText) + unitCost * count).toFixed(0);
 }
 
 function removeCartItem(id) {
+    const cartItem = document.getElementById(`item-${id}`);
     // se remueve del DOM
-    document.getElementById(`item-${id}`).remove();
+    cartItem.remove();
     // se remueve del localStorage
     const cart = JSON.parse(localStorage.getItem("cartProducts"));
     delete cart[id];
     // se vuelve a guardar el json actualizado
     localStorage.setItem("cartProducts", JSON.stringify(cart));
+    // se actualiza el valor del subtotal
+    subtotal.innerText = 
+        parseInt(subtotal.innerText) - 
+        // se obtiene el valor anterior del span
+        parseInt(cartItem.lastElementChild.previousElementSibling.firstElementChild.innerText);
+    // actualizar el resto de valores
+    updateTotalAndShippingCost();
 }
 
-function calcSubtotal(el, unitCost) {
-    if (el.value < 1) {
-        el.value = 1;
+function updateTotalAndShippingCost() {
+    let shippingCostValue = 0;
+    let checked = null;
+    // chequeamos que haya un metodo de envio clickeado
+    for (const t of shippingType)
+        if (t.checked) {
+            checked = t;
+            break;
+        }
+    // si lo hay se actualiza el costo de envio y el total
+    if (checked) {
+        shippingCostValue =
+            ( parseFloat(subtotal.innerText) * parseFloat(checked.value) ).toFixed(0);
+        shippingCost.innerText = shippingCostValue;
+    } 
+    total.innerText = 
+        ( parseFloat(shippingCostValue) + parseFloat(subtotal.innerText) ).toFixed(0);
+}
+
+function updateSubtotal(input, unitCost) {
+    if (input.value < 1) {
+        input.value = 1;
     }
-    el.parentNode.nextElementSibling.firstElementChild.innerText = unitCost * el.value;
+    const span = input.parentNode.nextElementSibling.firstElementChild;
+    console.log(span);
+    // se setea el subtotal asociado
+    span.innerText = unitCost * input.value;
+    // se actualiza el subtotal general
+    subtotal.innerText = ( parseFloat(subtotal.innerText) + 
+        // diferencia nuevo valor - valor anterior
+        (parseFloat(span.innerText) - parseFloat(span.dataset.oldvalue)) ).toFixed(0);
+    // actualizo el atributo valor anterior
+    span.dataset.oldvalue = span.innerText;
+
+    updateTotalAndShippingCost();
 }
 
-getCart(25801)
-    .then(response => {
-        response.data.articles.forEach(e => {
-            addRow(e);
-        });
-
-        const cart = JSON.parse(localStorage.getItem("cartProducts"));
-        for (const prod in cart)
-            addRow(cart[prod]);
-    });
-
-// TODO
-const subtotal = document.getElementById("subtotal");
-function updateSubtotalGeneral(element) {
-    console.log(parseInt(subtotal.innerText), - parseInt(element.oldValue), parseInt(element.innerText));
-    // nuevo subtotal es el subtotal anterior mas la diferencia entre el nuevo valor y el anterior
-    subtotal.innerText = parseInt(subtotal.innerText) - parseInt(element.oldValue) + parseInt(element.innerText);
-    // se actualiza el valor anterior
-    element.oldValue = element.innerText;
+for (const t of shippingType){
+    t.addEventListener("change", updateTotalAndShippingCost);
 }
-
-const paymenyMethod = document.getElementsByName("paymentMethod");
-const paymentMethodMsg = document.getElementById("paymentMethodmsg");
-const creditCardInputs = document.getElementsByClassName("cc-info");
-const bankTransferInputs = document.getElementsByClassName("bt-info");
-
-const successIcon = document.createElement("i");
-successIcon.classList.add("fas", "fal", "fa-check-circle", "ps-2");
-successIcon.style.color = "#00e03c";
-const errorIcon = document.createElement("i");
-errorIcon.classList.add("fas", "fal", "fa-exclamation-circle", "ps-2");
-errorIcon.style.color = "#ff0602";
 
 for (const p of paymenyMethod)
     p.addEventListener("change", () => {
         // el primer metodo de pago es la tarjeta de credito
         const disable = paymenyMethod[0].checked ? false : true;
+        // habilitado o deshabilitado de campos segun el metodo seleccionado
         for (const input of creditCardInputs)
             input.disabled = disable;
         for (const input of bankTransferInputs)
             input.disabled = !disable;
+        // mensaje de seleccion exitosa
         paymentMethodMsg.innerText = disable ? "Transferencia Bancaria" : "Tarjeta de Credito";
         paymentMethodMsg.style.color = "green";
         paymentMethodMsg.appendChild(successIcon);
     });
 
-const alert = document.getElementById("successAlert"); 
-const forms = document.getElementsByClassName("needs-validation");
+getCart(25801)
+.then(response => {
+    response.data.articles.forEach(e => {
+        addRow(e);
+    });
+
+    const cart = JSON.parse(localStorage.getItem("cartProducts"));
+    for (const prod in cart)
+        addRow(cart[prod]);
+
+    updateTotalAndShippingCost();
+});
+
+
 document.getElementById("checkoutBtn").addEventListener("click", (e) => {
     /*
     Los campos calle, número y esquina, no podrán estar vacíos.             | DONE.
@@ -110,6 +154,7 @@ document.getElementById("checkoutBtn").addEventListener("click", (e) => {
     for (const form of forms) {
         if (form.checkValidity() === false) {
             valid = false;
+            // formulario de metodo de pago
             if (form.id === "pmForm") {
                     paymentMethodMsg.innerText = "Debe rellenar los campos";
                     paymentMethodMsg.style.color = "#ff0602";
@@ -121,13 +166,9 @@ document.getElementById("checkoutBtn").addEventListener("click", (e) => {
     if (valid) {
         alert.classList.remove("d-none");
         setTimeout( () => {
-            alert.classList.add("d-none");
-        }, 5000);
-
-        for (const form of forms) 
-            form.submit();
+            for (const form of forms) 
+                form.submit();
+            window.location.replace("main.html");
+        }, 3000);
     }
 });
-
-// TODO 
-// actualizar subtotal, total, etc en function de productos del carrito y tipo de envio
---
+++ js/structural/navbar-footer.js
@@ -0,0 +1,69 @@
+
+const navbar = `  <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-1">
+<div class="container">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav w-100 justify-content-between">
+      <li class="nav-item my-auto">
+        <a class="nav-link active" href="main.html">Inicio</a>
+      </li>
+      <li class="nav-item my-auto">
+        <a class="nav-link" href="categories.html">Categorías</a>
+      </li>
+      <li class="nav-item my-auto">
+        <a class="nav-link" href="sell.html">Vender</a>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-secondary bg-dark dropdown-toggle flex-row-container py-0" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                <img id="nav-usrpicture" src="">
+                <span id="nav-usremail" class="nav-link"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                <li><a class="dropdown-item d-flex justify-content-between" href="my-profile.html">
+                    Mi Perfil <i class="fas fa-user my-auto"></i>
+                    </a>
+                </li>
+                <li><a class="dropdown-item d-flex justify-content-between" href="cart.html">
+                    Mi Carrito <i class="fas fa-shopping-cart my-auto"> </i>
+                    </a>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item d-flex justify-content-between" id="cerrar-sesion" href="index.html">
+                    Cerrar Sesion <i class="fas fa-sign-out-alt my-auto"> </i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+`;
+document.body.insertAdjacentHTML("afterbegin", navbar);
+
+const footer = `  <footer class="text-muted">
+<div class="container">
+  <p class="float-end">
+    <a href="#">Volver arriba</a>
+  </p>
+  <p>Este sitio forma parte de <a href="https://jovenesaprogramar.edu.uy/" target="_blank">Jovenes a Programar</a> -
+    2022</p>
+  <p>Clickea <a target="_blank" href="Letra.pdf">aquí</a> para descargar la letra del obligatorio.</p>
+</div>
+</footer>`
+
+document.getElementsByTagName("main")[0].insertAdjacentHTML("afterend", footer);
+
+const picture = document.getElementById("nav-usrpicture");
+picture.src = localStorage.getItem("usrPicture");
+document.getElementById("nav-usremail").innerText = localStorage.getItem("usrEmail");
+
+document.getElementById("cerrar-sesion").addEventListener("click", () => {
+    localStorage.removeItem("usrPicture");
+    localStorage.removeItem("usrEmail");
+});
